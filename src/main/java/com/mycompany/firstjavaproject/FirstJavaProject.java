8d1912afae40d505e02a152d3c553cb3576e74a7
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2022-05-08 20:40:33 -0300

    Person class implemented.

--- src/main/java/com/mycompany/firstjavaproject/FirstJavaProject.java
+++ src/main/java/com/mycompany/firstjavaproject/FirstJavaProject.java
@@ -17,20 +17,22 @@ import java.util.Scanner;
 public class FirstJavaProject {
 
     public static void main(String[] args) throws ParseException {
-
-        Scanner scan = new Scanner(System.in);
         
-        System.out.println("Insert your name");
-        String name = scan.nextLine();
-
-        System.out.println("Insert your birthday (yyyy-mm-dd)");
-        LocalDate birthday = LocalDate.parse(scan.nextLine());
-
-        scan.close();
-
-        int age = Period.between(birthday, LocalDate.now()).getYears();
-
+        String name;
+        LocalDate birthday;
+        
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Insert your name");
+            name = scan.nextLine();
+            System.out.println("Insert your birthday (yyyy-mm-dd)");
+            birthday = LocalDate.parse(scan.nextLine());
+        }
+        
+        Person person = new Person(name, birthday);
+        
+        person.CalculateAge();
+        
         System.out.println("Hello World!");
-        System.out.println("My name is " + name + ", I'm " + age + " years old.");
+        System.out.println("My name is " + name + ", I'm " + person.getAge() + " years old.");
     }
 }
